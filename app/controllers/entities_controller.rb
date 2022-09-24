5bf68afa063eea0a6305139c62f62320302c19f4
christian <christianbiringanine22@gmail.com>
2022-09-24 10:27:32 +0200

    Add the entities and groups controllers

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -1,2 +1,34 @@
 class EntitiesController < ApplicationController
+  def index
+    @group = Group.find(params[:group_id])
+    @entities = current_user.intities
+  end
+
+  def new
+    @group = Group.find(params[:group_id])
+  end
+
+  def create
+    @group = Group.find(params[:group_id])
+    @entity = @group.entities.build(entity_params)
+    @entity.user = current_user
+    if @entity.save
+      flash[:notice] = "Entity created Successfully!"
+      redirect_to user_group_entities_path(current_user, @group.id)
+    else
+      flash[:alert] = 'Ooops! Entity not created...'
+      render :new
+    end
+  end
+
+  def destroy
+    @group = Group.find(params[:group_id])
+    @entity = Entity.find(params[:id])
+    @entity.destroy
+    redirect_to user_group_entities_path(current_user, @group)
+  end
+
+  def entity_params
+    params.require(:entity).permit(:name, :amount)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,6 @@ Rails.application.routes.draw do
       resources :entities
     end
   end
-  # root to: "users#index"
+
   root to: "pages#home"
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
