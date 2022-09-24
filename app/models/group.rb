3b78cbd5fe32ba5240eada56669b33f08fff16d8
christian <christianbiringanine22@gmail.com>
2022-09-24 20:22:43 +0200

    Provide the default image if not one is provided by the user

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,5 +2,11 @@ class Group < ApplicationRecord
   belongs_to :user, foreign_key: 'user_id'
   has_many :entities, dependent: :destroy
 
-  validates :name, :icon, presence: true
+  validates :name, presence: true
+
+  # before_create :set_default_avatar
+
+  # def set_default_avatar
+  #   self.icon = 'default.jpg'
+  # end
 end
